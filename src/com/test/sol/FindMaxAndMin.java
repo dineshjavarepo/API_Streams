b8d7241bbec08cac4cfd74a80ddc7e5f7bb9cd31
Odem Dinesh <dineshodemdream1@gmail.com>
2026-06-04 08:15:44 +0530

    second_commit

--- src/com/test/sol/FindMaxAndMin.java
+++ src/com/test/sol/FindMaxAndMin.java
@@ -2,9 +2,12 @@ package com.test.sol;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class FindMaxAndMin {
@@ -23,7 +26,10 @@ public class FindMaxAndMin {
 		
 		List<Integer> uniqueList = Arrays.stream(arrys).boxed().filter(s->set.add(s)).collect(Collectors.toList());
 		
-		
+		Integer firstNonRepeating = Arrays.stream(arrys).boxed().collect(Collectors.groupingBy(
+                                       Function.identity(), LinkedHashMap:: new,Collectors.counting()))
+				                      .entrySet().stream().filter(s->s.getValue()==1)
+				                      .map(Map.Entry :: getKey).findFirst().orElse(null);
 		
 		
 		
